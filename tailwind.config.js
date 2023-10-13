62d85d822348f6a7d79513a118a3b60dd4473e4b
Maxim Latnikov <latnikou@gmail.com>
2023-10-13 19:57:46 +0300

    feat: finished first block with tailwind yay

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./src/index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#fe8a02',
+        'primary-dark': '#42413c', 
+        white: '#fff',
+        'white-50': '#d7ccc0',
+        light: '#f5f4f0',
+        dark: '#2a2626',
+        'dark-50': '#908d8b',
+        brown: '#b09a81',
+        blue: '#1085c5',
+      }
+    },
+    fontFamily: {
+      neucha: ['Neucha'],
+      opensans: ['Open Sans'],
+    }
   },
   plugins: [],
 }
